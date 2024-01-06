2cbf90ba430d579f3b3ae08479ed796b93ed107f
abdushakoor12 <abdushakoor009@yahoo.com>
2024-01-06 10:05:55 +0500

    minimal implementation of board

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +9,196 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Feynman Board',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BoardWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BoardWidget extends StatefulWidget {
+  const BoardWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BoardWidget> createState() => _BoardWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BoardWidgetState extends State<BoardWidget> {
+  List<DrawObject?> objects = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  double strokeWidth = 5;
+
+  Color color = Colors.black;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: GestureDetector(
+                onPanUpdate: (details) {
+                  RenderBox renderBox = context.findRenderObject() as RenderBox;
+                  final offset =
+                      renderBox.globalToLocal(details.globalPosition);
+
+                  setState(() {
+                    objects.add(DrawObject(offset, strokeWidth, color));
+                  });
+                },
+                onPanEnd: (details) {
+                  // adding a null value here to separate individual strokes
+                  setState(() {
+                    objects.add(null);
+                  });
+                },
+                child: CustomPaint(
+                  painter: DrawingPainter(objects),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Positioned(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ...[1, 2, 3, 4, 5].map((e) {
+                    final isSelected = strokeWidth == e.toDouble();
+                    return InkWell(
+                      onTap: () {
+                        setState(() {
+                          strokeWidth = e.toDouble();
+                        });
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.all(5),
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: isSelected ? Colors.blue : Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Text(
+                            e.toString(),
+                            style: TextStyle(
+                              color: isSelected ? Colors.white : Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    ...[
+                      Colors.red,
+                      Colors.green,
+                      Colors.blue,
+                      Colors.amber,
+                      Colors.black,
+                      Colors.white,
+                    ].map((e) {
+                      final isSelected = color == e;
+                      return InkWell(
+                        onTap: () {
+                          setState(() {
+                            color = e;
+                          });
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(5),
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: e,
+                            shape: BoxShape.circle,
+                          ),
+                          child: isSelected
+                              ? const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Container(),
+                        ),
+                      );
+                    }).toList(),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          setState(() {
+            objects.clear();
+          });
+        },
+        child: const Icon(Icons.clear),
+      ),
     );
   }
+}
+
+class DrawingPainter extends CustomPainter {
+  final List<DrawObject?> objects;
+
+  DrawingPainter(this.objects);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < objects.length - 1; i++) {
+      if (objects[i] != null && objects[i + 1] != null) {
+        DrawObject currentObject = objects[i]!;
+        DrawObject nextObject = objects[i + 1]!;
+
+        canvas.drawLine(currentObject.offset, nextObject.offset,
+            _getPaint(currentObject.color, currentObject.strokeWidth));
+      } else if (objects[i] != null && objects[i + 1] == null) {
+        DrawObject drawObject = objects[i]!;
+        canvas.drawPoints(PointMode.points, [drawObject.offset],
+            _getPaint(drawObject.color, drawObject.strokeWidth));
+      }
+    }
+  }
+
+  Paint _getPaint(Color color, double strokeWidth) {
+    return Paint()
+      ..color = color
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = strokeWidth;
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class DrawObject {
+  final Offset offset;
+  final double strokeWidth;
+  final Color color;
+
+  DrawObject(this.offset, this.strokeWidth, this.color);
 }
