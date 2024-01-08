456a82dd68c0675cbcd436fe7429aef3279686c9
abdushakoor12 <abdushakoor009@yahoo.com>
2024-01-08 08:06:18 +0500

    Convert drawObject to a sealed class

--- lib/features/draw/domain/entities/shapes/draw_object.dart
+++ lib/features/draw/domain/entities/shapes/draw_object.dart
@@ -1,9 +1,32 @@
 import 'dart:ui';
 
-class DrawObject {
-  final Offset offset;
+sealed class DrawObject {
   final double strokeWidth;
   final Color color;
 
-  DrawObject(this.offset, this.strokeWidth, this.color);
+  DrawObject(this.strokeWidth, this.color);
+}
+
+class LineObject extends DrawObject {
+  final List<Offset?> points;
+
+  LineObject(this.points, double strokeWidth, Color color) : super(strokeWidth, color);
+
+  LineObject copyWith({
+    List<Offset?>? points,
+    double? strokeWidth,
+    Color? color,
+  }) {
+    return LineObject(
+      points ?? this.points,
+      strokeWidth ?? this.strokeWidth,
+      color ?? this.color,
+    );
+  }
+}
+
+class RectangleObject extends DrawObject {
+  final Rect rect;
+
+  RectangleObject(this.rect, double strokeWidth, Color color) : super(strokeWidth, color);
 }
