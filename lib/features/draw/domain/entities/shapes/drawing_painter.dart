bc32a5692a46cbdd3711ecd2fdb0991021a3c982
abdushakoor12 <abdushakoor009@yahoo.com>
2024-01-09 20:17:10 +0500

    refactor painting details into shape classes

--- lib/features/draw/domain/entities/shapes/drawing_painter.dart
+++ lib/features/draw/domain/entities/shapes/drawing_painter.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 
 import 'draw_object.dart';
 
-class DrawingPainter extends CustomPainter with Brush {
+class DrawingPainter extends CustomPainter {
   final List<DrawObject> objects;
 
   DrawingPainter(this.objects);
@@ -13,36 +13,7 @@ class DrawingPainter extends CustomPainter with Brush {
   @override
   void paint(Canvas canvas, Size size) {
     for (DrawObject object in objects) {
-      switch (object) {
-        case PenObject():
-          final points = object.points;
-          for (int i = 0; i < points.length - 1; i++) {
-            if (points[i] != null && points[i + 1] != null) {
-              canvas.drawLine(points[i]!, points[i + 1]!,
-                  getPaint(object.color, object.strokeWidth));
-            } else if (points[i] != null && points[i + 1] == null) {
-              canvas.drawPoints(PointMode.points, [points[i]!],
-                  getPaint(object.color, object.strokeWidth));
-            }
-          }
-          break;
-        case RectangleObject():
-          canvas.drawRect(
-              object.rect, getOutlinedPaint(object.color, object.strokeWidth));
-          break;
-        case OvalObject():
-          canvas.drawOval(
-              object.rect, getOutlinedPaint(object.color, object.strokeWidth));
-          break;
-        case LineObject():
-          canvas.drawLine(object.startPoint, object.endPoint,
-              getPaint(object.color, object.strokeWidth));
-          break;
-        case CircleObject():
-          canvas.drawCircle(object.center, object.radius,
-              getOutlinedPaint(object.color, object.strokeWidth));
-          break;
-      }
+      object.paint(canvas);
     }
   }
 
